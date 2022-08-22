60ca097960d444b239458feb6df677075fc15b24
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-08-22 17:27:13 -0500

    Add files via upload

--- src/data/iconCards.js
+++ src/data/iconCards.js
@@ -3,12 +3,27 @@ const data = [
   {
     title: 'dashboards.trackingProcess',
     icon: 'iconsminds-mail-search',
-    value: 0,
+    value: 0
   },
   {
     title: 'dashboards.inactiveProcess',
     icon: 'iconsminds-inbox-empty',
-    value: 0,
-  },
+    value: 0
+  }
 ];
+
 export default data;
+
+export const membership = [
+  { title: 'Estado de su membresia', icon: 'iconsminds-receipt-4', value: '' },
+  {
+    title: 'Estado de tu proxima factura',
+    icon: 'iconsminds-credit-card-3',
+    value: ''
+  },
+  {
+    title: 'Fecha de vencimiento',
+    icon: 'iconsminds-calendar-1',
+    value: ''
+  }
+];
