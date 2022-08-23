c282769ce282f2b9c85fce4c8249be6a9833833a
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-08-23 17:46:18 -0500

    Add files via upload

--- src/containers/pages/ListPageListing.js
+++ src/containers/pages/ListPageListing.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import { Row } from 'reactstrap';
+import { Alert, Badge, Row } from 'reactstrap';
+import React, { useEffect, useState } from 'react';
+import clienteAxios from 'config/axios';
+import { Colxx } from 'components/common/CustomBootstrap';
+
+import IntlMessages from 'helpers/IntlMessages';
 import Pagination from './Pagination';
 import ContextMenuContainer from './ContextMenuContainer';
 import DataListView from './DataListView';
@@ -10,17 +14,71 @@ function collect(props) {
 
 const ListPageListing = ({
   items,
-
+  authUser,
   selectedItems,
   onCheckItem,
   currentPage,
   totalPage,
   onContextMenuClick,
   onContextMenu,
-  onChangePage,
+  onChangePage
 }) => {
+  const [approved, setApproved] = useState(false);
+
+  const { currentUser } = authUser;
+
+  useEffect(() => {
+    const getPayment = async () => {
+      try {
+        const paymentData = await clienteAxios.get(
+          `/payments/lawyer/${currentUser.id}`
+        );
+
+        const countPayment = paymentData.data.length - 1;
+        console.log(paymentData.data[countPayment]);
+
+        if (paymentData.data[countPayment].status.includes('approved')) {
+          setApproved(true);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getPayment();
+    console.log(approved);
+  }, []);
+
+  const onClick = () => {
+    window.location.href = '/app/blank-page/';
+  };
+
   return (
     <Row>
+      {approved ? null : (
+        <Colxx xxs='12'>
+          <Alert
+            color='danger'
+            className='rounded'
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}
+          >
+            <IntlMessages id='Su membresia se encuentra vencida, para continuar haciendo segumiento a sus casos debera realizar el pago de su factura' />
+            <Badge
+              color='danger'
+              pill
+              onClick={onClick}
+              style={{ cursor: 'pointer' }}
+            >
+              REALIZAR PAGO
+            </Badge>
+          </Alert>
+        </Colxx>
+      )}
+
       {items.map((product) => {
         return (
           <DataListView
