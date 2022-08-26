b2decbaf8cd223f66687d9597e65d53bdd3bdffc
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-08-26 09:31:22 -0500

    Add files via upload

--- src/views/user/login.js
+++ src/views/user/login.js
@@ -51,81 +51,82 @@ const Login = ({ history, loading, error, loginUserAction }) => {
   const initialValues = { email, password };
 
   return (
-    <Row className="h-100">
-      <Colxx xxs="12" md="10" className="mx-auto my-auto">
-        <Card className="auth-card">
-          <div className="position-relative image-side ">
-            <p className="text-white h2">MVP APP PROCESS</p>
-            <p className="white mb-0">
-              Please use your credentials to login.
+    <Row className='h-100'>
+      <Colxx xxs='12' md='10' className='mx-auto my-auto'>
+        <Card className='auth-card'>
+          <div className='position-relative image-side '>
+            <p className='text-white h2'>MVP APP PROCESS</p>
+            <p className='white mb-0'>
+              Ingresa tus datos para iniciar sesión.
               <br />
-              If you are not a member, please{' '}
-              <NavLink to="/user/register" className="white">
-                register
+              Si aun no estas registrado, haz clic aqui en{' '}
+              <NavLink to='/user/register' style={{ color: 'white' }}>
+                registrarte
               </NavLink>
               .
             </p>
           </div>
-          <div className="form-side">
-            <NavLink to="/" className="white">
-              <span className="logo-single" />
+          <div className='form-side'>
+            <NavLink to='/' className='white'>
+              <span className='logo-single' />
             </NavLink>
-            <CardTitle className="mb-4">
-              <IntlMessages id="user.login-title" />
+            <CardTitle className='mb-4'>
+              <IntlMessages id='user.login-title' />
             </CardTitle>
 
             <Formik initialValues={initialValues} onSubmit={onUserLogin}>
               {({ errors, touched }) => (
-                <Form className="av-tooltip tooltip-label-bottom">
-                  <FormGroup className="form-group has-float-label">
+                <Form className='av-tooltip tooltip-label-bottom'>
+                  <FormGroup className='form-group has-float-label'>
                     <Label>
-                      <IntlMessages id="user.email" />
+                      <IntlMessages id='user.email' />
                     </Label>
                     <Field
-                      className="form-control"
-                      name="email"
+                      className='form-control'
+                      name='email'
                       validate={validateEmail}
                     />
                     {errors.email && touched.email && (
-                      <div className="invalid-feedback d-block">
+                      <div className='invalid-feedback d-block'>
                         {errors.email}
                       </div>
                     )}
                   </FormGroup>
-                  <FormGroup className="form-group has-float-label">
+                  <FormGroup className='form-group has-float-label'>
                     <Label>
-                      <IntlMessages id="user.password" />
+                      <IntlMessages id='user.password' />
                     </Label>
                     <Field
-                      className="form-control"
-                      type="password"
-                      name="password"
+                      className='form-control'
+                      type='password'
+                      name='password'
                       validate={validatePassword}
                     />
                     {errors.password && touched.password && (
-                      <div className="invalid-feedback d-block">
+                      <div className='invalid-feedback d-block'>
                         {errors.password}
                       </div>
                     )}
                   </FormGroup>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <NavLink to="/user/forgot-password">
-                      <IntlMessages id="user.forgot-password-question" />
+                  <div className='d-flex justify-content-between align-items-center'>
+                    <NavLink to='/user/forgot-password'>
+                      <IntlMessages id='user.forgot-password-question' />
                     </NavLink>
+
                     <Button
-                      color="primary"
+                      color='primary'
                       className={`btn-shadow btn-multiple-state ${
                         loading ? 'show-spinner' : ''
                       }`}
-                      size="lg"
+                      size='lg'
                     >
-                      <span className="spinner d-inline-block">
-                        <span className="bounce1" />
-                        <span className="bounce2" />
-                        <span className="bounce3" />
+                      <span className='spinner d-inline-block'>
+                        <span className='bounce1' />
+                        <span className='bounce2' />
+                        <span className='bounce3' />
                       </span>
-                      <span className="label">
-                        <IntlMessages id="user.login-button" />
+                      <span className='label'>
+                        <IntlMessages id='user.login-button' />
                       </span>
                     </Button>
                   </div>
@@ -144,5 +145,5 @@ const mapStateToProps = ({ authUser }) => {
 };
 
 export default connect(mapStateToProps, {
-  loginUserAction: loginUser,
+  loginUserAction: loginUser
 })(Login);
--- src/views/user/register.js
+++ src/views/user/register.js
@@ -7,7 +7,7 @@ import {
   FormGroup,
   Label,
   Input,
-  Button,
+  Button
 } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -16,8 +16,9 @@ import { registerUser } from 'redux/actions';
 import IntlMessages from 'helpers/IntlMessages';
 import { Colxx } from 'components/common/CustomBootstrap';
 import { adminRoot } from 'constants/defaultValues';
+import { NotificationManager } from 'components/common/react-notifications';
 
-const Register = ({ history, registerUserAction }) => {
+const Register = ({ loading, error, history, registerUserAction }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
@@ -30,87 +31,117 @@ const Register = ({ history, registerUserAction }) => {
     if (email === 'j') {
       history.push(adminRoot);
     }
+
+    if (error) {
+      NotificationManager.warning(
+        error,
+        'Register Failed',
+        3000,
+        null,
+        null,
+        ''
+      );
+    } else {
+      NotificationManager.success(
+        'Ya puedes iniciar sesión',
+        'Register Success',
+        3000,
+        null,
+        null,
+        ''
+      );
+    }
+
     // call registerUserAction()
   };
 
   return (
-    <Row className="h-100">
-      <Colxx xxs="12" md="10" className="mx-auto my-auto">
-        <Card className="auth-card">
-          <div className="position-relative image-side ">
-            <p className="text-white h2">MVP APP PROCESS</p>
-            <p className="white mb-0">
-              Please use this form to register. <br />
-              If you are a member, please{' '}
-              <NavLink to="/user/login" className="white">
+    <Row className='h-100'>
+      <Colxx xxs='12' md='10' className='mx-auto my-auto'>
+        <Card className='auth-card'>
+          <div className='position-relative image-side '>
+            <p className='text-white h2'>MVP APP PROCESS</p>
+            <p className='white mb-0'>
+              Llena este formulario con tus datos para registrarte. <br />
+              Si ya tienes una cuenta, haz clic aqui en{' '}
+              <NavLink to='/user/login' className='white'>
                 login
               </NavLink>
               .
             </p>
           </div>
-          <div className="form-side">
-            <NavLink to="/" className="white">
-              <span className="logo-single" />
+          <div className='form-side'>
+            <NavLink to='/' className='white'>
+              <span className='logo-single' />
             </NavLink>
-            <CardTitle className="mb-4">
-              <IntlMessages id="user.register" />
+            <CardTitle className='mb-4'>
+              <IntlMessages id='user.register' />
             </CardTitle>
             <Form>
-              <FormGroup className="form-group has-float-label  mb-4">
+              <FormGroup className='form-group has-float-label  mb-4'>
                 <Label>
-                  <IntlMessages id="user.fullname" />
+                  <IntlMessages id='user.fullname' />
                 </Label>
                 <Input
-                  type="name"
+                  type='name'
                   defaultValue={name}
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
               </FormGroup>
 
-              <FormGroup className="form-group has-float-label  mb-4">
+              <FormGroup className='form-group has-float-label  mb-4'>
                 <Label>
-                  <IntlMessages id="user.email" />
+                  <IntlMessages id='Telefono' />
                 </Label>
                 <Input
-                  type="email"
-                  defaultValue={email}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </FormGroup>
-
-              <FormGroup className="form-group has-float-label  mb-4">
-                <Label>
-                  <IntlMessages id="Telefono" />
-                </Label>
-                <Input
-                  type="phone"
+                  type='phone'
                   defaultValue={phone}
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
                 />
               </FormGroup>
 
-              <FormGroup className="form-group has-float-label  mb-4">
+              <FormGroup className='form-group has-float-label  mb-4'>
                 <Label>
-                  <IntlMessages id="user.password" defaultValue={password} />
+                  <IntlMessages id='user.email' />
                 </Label>
                 <Input
-                  type="password"
+                  type='email'
+                  defaultValue={email}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup className='form-group has-float-label  mb-4'>
+                <Label>
+                  <IntlMessages id='user.password' defaultValue={password} />
+                </Label>
+                <Input
+                  type='password'
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </FormGroup>
 
-              <div className="d-flex justify-content-end align-items-center">
+              <div className='d-flex justify-content-end align-items-center'>
                 <Button
-                  color="primary"
-                  className="btn-shadow"
-                  size="lg"
+                  color='primary'
+                  className={`btn-shadow btn-multiple-state ${
+                    loading ? 'show-spinner' : ''
+                  }`}
+                  size='lg'
                   onClick={() => onUserRegister()}
                 >
-                  <IntlMessages id="user.register-button" />
+                  <span className='spinner d-inline-block'>
+                    <span className='bounce1' />
+                    <span className='bounce2' />
+                    <span className='bounce3' />
+                  </span>
+                  <span className='label'>
+                    <IntlMessages id='user.register-button' />
+                  </span>
                 </Button>
               </div>
             </Form>
@@ -120,8 +151,11 @@ const Register = ({ history, registerUserAction }) => {
     </Row>
   );
 };
-const mapStateToProps = () => {};
+const mapStateToProps = ({ authUser }) => {
+  const { loading, error } = authUser;
+  return { loading, error };
+};
 
 export default connect(mapStateToProps, {
-  registerUserAction: registerUser,
+  registerUserAction: registerUser
 })(Register);
