335cef1aedf2c01a2727f05a251168e2b9e844da
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-10-19 19:43:06 -0500

    Add files via upload

--- src/components/CalendarToolbar.js
+++ src/components/CalendarToolbar.js
@@ -18,40 +18,40 @@ const CalendarToolbar = (toolbar) => {
     const date = moment(toolbar.date);
     return (
       <span>
-        <span>{date.format('MMMM')} </span>
+        <span>{date.format('MMM')} </span>
         <span> {date.format('YYYY')}</span>
       </span>
     );
   };
 
   return (
-    <div className="big-calendar-header">
-      <div className="float-left">
+    <div className='big-calendar-header'>
+      <div className='float-left'>
         <label>{label()}</label>
       </div>
 
-      <div className="float-right">
+      <div className='float-right'>
         <div>
           <button
-            type="button"
-            className="btn btn-primary calendar-today-btn mr-2"
+            type='button'
+            className='btn btn-primary calendar-today-btn mr-2'
             onClick={goToCurrent}
           >
             Today
           </button>
           <button
-            type="button"
-            className="btn calendar-prev-btn mr-1"
+            type='button'
+            className='btn calendar-prev-btn mr-1'
             onClick={goToBack}
           >
-            <span className="simple-icon-arrow-left" />
+            <span className='simple-icon-arrow-left' />
           </button>
           <button
-            type="button"
-            className="btn calendar-next-btn"
+            type='button'
+            className='btn calendar-next-btn'
             onClick={goToNext}
           >
-            <span className="simple-icon-arrow-right" />
+            <span className='simple-icon-arrow-right' />
           </button>
         </div>
       </div>
