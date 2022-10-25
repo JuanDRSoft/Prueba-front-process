ee14a9e5a36b2c1c268b7dcc7c2cda00e7779cd0
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-10-25 08:38:09 -0500

    Add files via upload

--- src/views/app/detail/components/eventos/PreviewEventos.js
+++ src/views/app/detail/components/eventos/PreviewEventos.js
@@ -1,22 +1,49 @@
 import { Colxx } from 'components/common/CustomBootstrap';
-import React from 'react';
+import ModalEvent from 'containers/pages/ModalEvent';
+import React, { useState } from 'react';
 import { Card, CardBody, CardTitle, Row } from 'reactstrap';
 
-const PreviewEventos = ({ event }) => {
-  const { title, creado, process, start, end } = event;
+const PreviewEventos = ({ event, events, setEvents }) => {
+  const [modalOpenEvent, setModalOpenEvent] = useState(false);
+
+  const { title, creado, process, start, end, _id } = event;
 
   const fecha = creado.split('T')[0];
+
+  const endEvento = end;
+
+  const handleOpenModalEvent = () => {
+    setModalOpenEvent(!modalOpenEvent);
+  };
 
   return (
     <Row className='mt-2'>
       <Colxx>
         <Card>
-          <CardBody>
+          <CardBody className='pb-0'>
             <CardTitle
-              style={{ display: 'flex', justifyContent: 'space-between' }}
+              style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+              }}
             >
               {title}
-              <p>{fecha}</p>
+              <button
+                type='button'
+                className='btn btn-link'
+                style={{
+                  fontSize: 13,
+                  marginRight: -20,
+                  marginTop: -35,
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 5
+                }}
+                onClick={handleOpenModalEvent}
+              >
+                <i className='simple-icon-note' /> Editar
+              </button>
             </CardTitle>
 
             <div style={{ display: 'flex' }}>
@@ -27,19 +54,51 @@ const PreviewEventos = ({ event }) => {
                 {start.split('T')[1].split(':00.')[0]}{' '}
                 {start.split('T')[1].split(':')[0] > 11 ? 'pm' : 'am'}
               </p>
-              <p className='ml-2 mr-2'>|</p>
-              <p>
-                Termina: {end.split('T')[0]}
-                {' / '}
-                {end.split('T')[1].split(':00.')[0]}{' '}
-                {end.split('T')[1].split(':')[0] > 11 ? 'pm' : 'am'}
-              </p>
+              {end && (
+                <div style={{ display: 'flex' }}>
+                  <p className='ml-2 mr-2'>|</p>{' '}
+                  <p>
+                    Termina: {end.split('T')[0]}
+                    {' / '}
+                    {end.split('T')[1].split(':00.')[0]}{' '}
+                    {end.split('T')[1].split(':')[0] > 11 ? 'pm' : 'am'}
+                  </p>
+                </div>
+              )}
             </div>
 
             {process && <p>Proceso enlazado: {process}</p>}
+
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'end',
+                alignItems: 'center'
+              }}
+            >
+              <p
+                style={{
+                  fontSize: 10,
+                  color: 'gray',
+                  textAlign: 'end'
+                }}
+              >
+                Creado: {fecha}
+              </p>
+            </div>
           </CardBody>
         </Card>
       </Colxx>
+
+      <ModalEvent
+        handleOpenModalEvent={handleOpenModalEvent}
+        modalOpenEvent={modalOpenEvent}
+        setEvents={setEvents}
+        events={events}
+        event={event}
+        id={_id}
+        endEvento={endEvento}
+      />
     </Row>
   );
 };
