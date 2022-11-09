ba4b514a8a5f0a125400419a4c6a263ca5e38e68
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-11-09 11:01:59 -0500

    Add files via upload

--- src/containers/navs/Topnav.Calendar.js
+++ src/containers/navs/Topnav.Calendar.js
@@ -1,25 +1,40 @@
+import clienteAxios from 'config/axios';
 import ModalCalendar from 'containers/pages/ModalCalendar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'reactstrap';
 
 const TopNavCalendar = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const getEvent = async () => {
+      const processData = await clienteAxios.get('/event/all/bylawyer');
+      console.log('Data', processData);
+      setEvents(processData.data);
+    };
+
+    getEvent();
+  }, []);
+
+  const eventos = events.filter((e) => new Date(e.start) > Date.now());
 
   const handleOpenModal = () => {
     setModalOpen(!modalOpen);
   };
   return (
-    <>
+    <div className='position-relative d-inline-block'>
       <Button
         className='header-icon notificationButton'
         color='empty'
         onClick={handleOpenModal}
       >
         <i className='simple-icon-calendar' />
+        <span className='count'>{eventos.length}</span>
       </Button>
 
       <ModalCalendar modalOpen={modalOpen} handleOpenModal={handleOpenModal} />
-    </>
+    </div>
   );
 };
 
