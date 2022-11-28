f155ffa13af667050dd71060e2db8ee35b3ac789
JuanDRSoft <95110527+JuanDRSoft@users.noreply.github.com>
2022-11-28 13:06:48 -0500

    Add files via upload

--- src/containers/dashboards/Calendar.js
+++ src/containers/dashboards/Calendar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, CardBody } from 'reactstrap';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -6,11 +6,41 @@ import moment from 'moment';
 import CalendarToolbar from 'components/CalendarToolbar';
 
 import { getDirection } from 'helpers/Utils';
+import DetailEventModal from 'containers/pages/DetailEventModal';
+import PreviewEventos from 'views/app/detail/components/eventos/PreviewEventos';
+
 // import data from 'data/events';
 
 const localizer = momentLocalizer(moment);
 
-const CalendarCard = ({ events }) => {
+const CalendarCard = ({ events, process, setEvents }) => {
+  const [event, setEvent] = useState('');
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const modalOpenAction = () => {
+    setModalOpen(!modalOpen);
+  };
+
+  // eslint-disable-next-line no-underscore-dangle
+  const evento = events.filter((e) => e._id === event._id);
+  const dataEvent = () => {
+    return evento.map((e) => (
+      <PreviewEventos
+        event={e}
+        key={e}
+        process={process}
+        events={events}
+        setEvents={setEvents}
+      />
+    ));
+  };
+
+  useEffect(() => {
+    if (event !== '') {
+      modalOpenAction();
+    }
+  }, [event]);
+
   return (
     <Card>
       <CardBody>
@@ -23,8 +53,17 @@ const CalendarCard = ({ events }) => {
           components={{
             toolbar: CalendarToolbar
           }}
+          onSelectEvent={(e) => setEvent(e)}
         />
       </CardBody>
+
+      <DetailEventModal
+        modalOpen={modalOpen}
+        event={event}
+        modalOpenAction={modalOpenAction}
+        process={process}
+        dataEvent={dataEvent}
+      />
     </Card>
   );
 };
